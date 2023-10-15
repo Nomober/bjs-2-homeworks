4275a2375c4bbd708a5f21d2c40872620c827110
Nomober <erimihanov@mail.ru>
2023-10-15 21:17:33 +0300

    First exercise ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c
+
+	if (d > 0) {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a)
+		let x2 = (-b - Math.sqrt(d)) / (2 * a)
+		arr.push(x1, x2)
+	} else if (d === 0) {
+		let x = -b / (2 * a)
+		arr.push(x)
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent = percent / 100;
+	let S = amount - contribution;
+	let n = countMonths;
+	let P = percent / 12;
+	let paymentMonth = S * (P + (P / (((1 + P) ** n) - 1)));
+	let paymentAll = paymentMonth * countMonths;
+
+	return Number(paymentAll.toFixed(2));
 }
