fcd047ba5405fe4df5ac1a334f87a24557a9d9b8
Nomober <erimihanov@mail.ru>
2023-11-12 12:41:51 +0300

    Третье задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index] )
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter((user) => user.gender === gender).map(userAge => userAge.age).reduce((acc, age, index, array) => acc + age/array.length, 0)
 }
